ed0e1159d6ffe6a5517ea44ef78f04775abcc657
alekate <alekate2003@hotmail.com>
2023-10-06 19:04:31 -0300

    Nivel 1 rediseño
    con sprites nuevos y colliders apopiados

--- VaporLock/Assets/Scripts/camaraFollow.cs
+++ VaporLock/Assets/Scripts/camaraFollow.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class camaraFollow : MonoBehaviour
 {
     public Transform target;
+    //public GameObject player;
+    //private Vector3 offset;      
+    
 
     private void LateUpdate(){
         transform.position = target.position;
+        //offset = transform.position;
+        //offset = transform.position - player.transform.position;
+
     }
 
+   // void lateUpdate(){
+        //transform.position = player.transform.position + offset;
+   // }
 
 
 
